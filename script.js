f983194403d51e584ca4b3d192efb2bd35a2bc6e
Tomson <twesolowski61@gmail.com>
2021-02-03 10:21:24 +0100

    .flat and flatMap practice

--- script.js
+++ script.js
@@ -387,14 +387,31 @@ btnTransfer.addEventListener('click', function (e) {
 
 // const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 // console.log(account);
-console.log(movements);
-console.log(movements.includes(-130));
+// console.log(movements);
+// console.log(movements.includes(-130));
 
-const anyDeposits = movements.some(mov => mov > 1500);
-console.log(anyDeposits);
+// const anyDeposits = movements.some(mov => mov > 1500);
+// console.log(anyDeposits);
 
-console.log(movements.every(mov => mov > 0));
-console.log(account4.movements.every(mov => mov > 0));
+// console.log(movements.every(mov => mov > 0));
+// console.log(account4.movements.every(mov => mov > 0));
 
-const deposit = mov => mov > 0;
-console.log(movements.some(deposit));
+// const deposit = mov => mov > 0;
+// console.log(movements.some(deposit));
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
