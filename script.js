f22a097ebd5aa88905759271783b047e9e2c3ca0
Tomson <twesolowski61@gmail.com>
2021-02-04 17:45:50 +0100

    creating and filling arrays practice

--- script.js
+++ script.js
@@ -425,26 +425,52 @@ btnSort.addEventListener('click', function (e) {
 //   .flatMap(acc => acc.movements)
 //   .reduce((acc, mov) => acc + mov, 0);
 // console.log(overalBalance);
-const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
-console.log(owners.sort());
-console.log(owners);
+// const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+// console.log(owners.sort());
+// console.log(owners);
 
-console.log(movements);
+// console.log(movements);
 
-// return < 0, A,B (keep order)
-//return > 0, B,A (switch order)
-// Ascending
-// movements.sort((a, b) => {
-//   if (a > b) return 1;
-//   if (b > a) return -1;
-// });
-movements.sort((a, b) => a - b);
-console.log(movements);
+// // return < 0, A,B (keep order)
+// //return > 0, B,A (switch order)
+// // Ascending
+// // movements.sort((a, b) => {
+// //   if (a > b) return 1;
+// //   if (b > a) return -1;
+// // });
+// movements.sort((a, b) => a - b);
+// console.log(movements);
 
-// Descending
-// movements.sort((a, b) => {
-//   if (a > b) return -1;
-//   if (b > a) return 1;
-// });
-movements.sort((a, b) => b - a);
-console.log(movements);
+// // Descending
+// // movements.sort((a, b) => {
+// //   if (a > b) return -1;
+// //   if (b > a) return 1;
+// // });
+// movements.sort((a, b) => b - a);
+// console.log(movements);
+const arr = [1, 2, 3, 4, 5, 6, 7];
+const x = new Array(7);
+console.log(x);
+x.fill(7);
+console.log(x);
+arr.fill(23, 4, 6);
+console.log(arr);
+
+const u = Array.from({ length: 7 }, () => 1);
+console.log(u);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+const dice = Array.from(
+  { length: 100 },
+  () => 1 + Math.floor(Math.random() * 6)
+);
+console.log(dice);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value')
+  );
+  console.log(movementsUI.map(el => el.textContent.replace('€', '')));
+});
