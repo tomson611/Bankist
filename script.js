42f21d33d9aefa3327445598100c2e6d97308b59
Tomson <twesolowski61@gmail.com>
2021-02-08 07:42:55 +0100

    coding challange #4

--- script.js
+++ script.js
@@ -502,3 +502,81 @@ const { deposits, withdrawals } = accounts
     { deposits: 0, withdrawals: 0 }
   );
 console.log(deposits, withdrawals);
+
+const convertTitleCase = function (title) {
+  const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word =>
+      exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
+    );
+  return titleCase;
+};
+
+console.log(convertTitleCase('this is a nice title'));
+
+// Your tasks:
+// 1. Loop over the 'dogs' array containing dog objects, and for each dog, calculate
+// the recommended food portion and add it to the object as a new property. Do
+// not create a new array, simply loop over the array. Forumla:
+// recommendedFood = weight ** 0.75 * 28. (The result is in grams of
+// food, and the weight needs to be in kg)
+// 2. Find Sarah's dog and log to the console whether it's eating too much or too
+// little. Hint: Some dogs have multiple owners, so you first need to find Sarah in
+// the owners array, and so this one is a bit tricky (on purpose) 🤓
+// 3. Create an array containing all owners of dogs who eat too much
+// ('ownersEatTooMuch') and an array with all owners of dogs who eat too little
+// ('ownersEatTooLittle').
+// 4. Log a string to the console for each array created in 3., like this: "Matilda and
+// Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat
+// too little!"
+// 5. Log to the console whether there is any dog eating exactly the amount of food
+// that is recommended (just true or false)
+// 6. Log to the console whether there is any dog eating an okay amount of food
+// (just true or false)
+// 7. Create an array containing the dogs that are eating an okay amount of food (try
+// to reuse the condition used in 6.)
+// 8. Create a shallow copy of the 'dogs' array and sort it by recommended food
+// portion in an ascending order (keep in mind that the portions are inside the
+// array's objects 😉)
+// The Complete JavaScript Course 26
+// Hints:
+// § Use many different tools to solve these challenges, you can use the summary
+// lecture to choose between them 😉
+// § Being within a range 10% above and below the recommended portion means:
+// current > (recommended * 0.90) && current < (recommended *
+// 1.10). Basically, the current portion should be between 90% and 110% of the
+// recommended portion.
+// Test data:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+dogs.map(dog => (dog.recommendedFood = dog.weight ** 0.75 * 28));
+
+dogs.map(dog => {
+  if (dog.owners.find(owner => owner === 'Sarah')) {
+    console.log(
+      dog.curFood < dog.recommendedFood
+        ? 'Dog eats too little'
+        : 'Dog eats too much'
+    );
+  }
+});
+console.log(dogs);
+const ownersEatTooMuch = [];
+const ownersEatTooLittle = [];
+
+dogs.map(dog =>
+  dog.curFood < dog.recommendedFood
+    ? ownersEatTooLittle.push(dog.owners)
+    : ownersEatTooMuch.push(dog.owners)
+);
+console.log(ownersEatTooMuch, ownersEatTooLittle);
+
+console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
