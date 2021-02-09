719abe002018873f4777a8efe7c803de9a217e3b
Tomson <twesolowski61@gmail.com>
2021-02-09 07:32:00 +0100

    Math practice

--- script.js
+++ script.js
@@ -611,29 +611,47 @@ btnSort.addEventListener('click', function (e) {
 // });
 
 // console.log(dogsSorted);
-console.log(23 === 23.0);
-console.log(0.1 + 0.2);
-console.log(0.1 + 0.2 === 0.3);
+// console.log(23 === 23.0);
+// console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2 === 0.3);
 
-//Conversion
-console.log(Number('23'));
-console.log(+'23');
+// //Conversion
+// console.log(Number('23'));
+// console.log(+'23');
 
-//Parsing
-console.log(Number.parseInt('30elo', 10));
-console.log(Number.parseFloat('2.5rem'));
+// //Parsing
+// console.log(Number.parseInt('30elo', 10));
+// console.log(Number.parseFloat('2.5rem'));
 
-console.log(Number.isNaN(20));
-console.log(Number.isNaN('20'));
-console.log(Number.isNaN(+'20X'));
-console.log(Number.isNaN(23 / 0));
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(+'20X'));
+// console.log(Number.isNaN(23 / 0));
 
-//Checking if value is a number
-console.log(Number.isFinite(20));
-console.log(Number.isFinite('20'));
-console.log(Number.isFinite(+'20x'));
-console.log(Number.isFinite(20 / 0));
+// //Checking if value is a number
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(+'20x'));
+// console.log(Number.isFinite(20 / 0));
 
-console.log(Number.isInteger(23.0));
-console.log(Number.isInteger(23));
-console.log(Number.isInteger(23 / 0));
+// console.log(Number.isInteger(23.0));
+// console.log(Number.isInteger(23));
+// console.log(Number.isInteger(23 / 0));
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
+console.log(Math.min(5, 18, '23', 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6 + 1));
+
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(10, 20));
