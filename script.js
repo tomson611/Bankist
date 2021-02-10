c52092c4bad538dad8d98cd9ec527e2f7ca37b31
Tomson <twesolowski61@gmail.com>
2021-02-10 08:20:47 +0100

    remainder operator practice

--- script.js
+++ script.js
@@ -638,38 +638,54 @@ btnSort.addEventListener('click', function (e) {
 // console.log(Number.isInteger(23));
 // console.log(Number.isInteger(23 / 0));
 
-console.log(Math.sqrt(25));
-console.log(25 ** (1 / 2));
-console.log(8 ** (1 / 3));
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
 
-console.log(Math.max(5, 18, 23, 11, 2));
-console.log(Math.max(5, 18, '23', 11, 2));
+// console.log(Math.max(5, 18, 23, 11, 2));
+// console.log(Math.max(5, 18, '23', 11, 2));
 
-console.log(Math.min(5, 18, 23, 11, 2));
-console.log(Math.min(5, 18, '23', 11, 2));
+// console.log(Math.min(5, 18, 23, 11, 2));
+// console.log(Math.min(5, 18, '23', 11, 2));
 
-console.log(Math.PI * Number.parseFloat('10px') ** 2);
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
 
-console.log(Math.trunc(Math.random() * 6 + 1));
+// console.log(Math.trunc(Math.random() * 6 + 1));
 
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min) + 1) + min;
-console.log(randomInt(10, 20));
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min) + 1) + min;
+// console.log(randomInt(10, 20));
 
-// Rounding integers
-console.log(Math.trunc(23.3));
-console.log(Math.round(23.9));
+// // Rounding integers
+// console.log(Math.trunc(23.3));
+// console.log(Math.round(23.9));
 
-console.log(Math.ceil(23.9));
-console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
+// console.log(Math.ceil(23.3));
 
-console.log(Math.floor(23.9));
-console.log(Math.floor('23.9'));
+// console.log(Math.floor(23.9));
+// console.log(Math.floor('23.9'));
 
-console.log(Math.trunc(23.9));
-console.log(Math.floor(-23.9));
+// console.log(Math.trunc(23.9));
+// console.log(Math.floor(-23.9));
 
-// Rounding decimals
-console.log((2.7).toFixed(0));
-console.log((2.7).toFixed(3));
-console.log(+(2.345).toFixed(2));
+// // Rounding decimals
+// console.log((2.7).toFixed(0));
+// console.log((2.7).toFixed(3));
+// console.log(+(2.345).toFixed(2));
+
+console.log(5 % 2);
+console.log(8 % 3);
+
+const isEven = n => n % 2 === 0;
+
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 == 0) row.style.backgroundColor = 'blue';
+  });
+});
